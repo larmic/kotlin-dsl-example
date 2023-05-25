f9786ead07f443b4efd5f4b9762440a107c28fab
larmic <mail@larmic.de>
2023-05-25 11:16:13 +0200

    feature[kotlin-dsl]: company and employee entity

--- src/test/kotlin/de/larmic/postgres/tools/TestDataFactories.kt
+++ src/test/kotlin/de/larmic/postgres/tools/TestDataFactories.kt
@@ -18,3 +18,25 @@ fun createEmployeeDto(
     name: String = "Donald Duck",
     email: String = "donald@duck.de",
 ) = CreateEmployeeDto(name = name, email = email)
+
+class EmployeeEntityBuilder {
+    var name: String = "Donald Duck"
+    var email: String = "donald@duck.de"
+
+    fun build(): EmployeeEntity = EmployeeEntity(name = name, email = email)
+}
+
+fun employee(block: EmployeeEntityBuilder.() -> Unit) = EmployeeEntityBuilder().apply(block).build()
+
+class CompanyEntityBuilder {
+    var name: String = "Entenhausen AG"
+    val employees: MutableList<EmployeeEntity> = mutableListOf()
+
+    fun address(block: EmployeeEntityBuilder.() -> Unit) {
+        employees.add(EmployeeEntityBuilder().apply(block).build())
+    }
+
+    fun build(): CompanyEntity = CompanyEntity(name = name, employees = employees)
+}
+
+fun company(block: CompanyEntityBuilder.() -> Unit) = CompanyEntityBuilder().apply(block).build()
